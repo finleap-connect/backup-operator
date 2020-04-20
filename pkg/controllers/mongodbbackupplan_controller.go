896ad607a53c8d57d3e22291fd5c967bcf37a6b5
Nik Voss <niklas.voss@gmail.com>
2020-04-20 11:06:23 +0200

    Implemented secret handling for mongodbbackupplan reconciler, status field now optional

--- pkg/controllers/mongodbbackupplan_controller.go
+++ pkg/controllers/mongodbbackupplan_controller.go
@@ -18,6 +18,8 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	backupv1alpha1 "github.com/kubism-io/backup-operator/api/v1alpha1"
 	"github.com/kubism-io/backup-operator/pkg/util"
@@ -26,13 +28,16 @@ import (
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
+	ref "k8s.io/client-go/tools/reference"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
-	finalizerName = "backup.kubism.io"
+	finalizerName   = "backup.kubism.io"
+	secretFieldName = "plan.json"
 )
 
 // MongoDBBackupPlanReconciler reconciles a MongoDBBackupPlan object
@@ -91,6 +96,62 @@ func (r *MongoDBBackupPlanReconciler) Reconcile(req ctrl.Request) (ctrl.Result, 
 	}
 
 	// TODO: validate plan
+
+	// First we create or update the secret before checking the related CronJob
+	var secret corev1.Secret
+	// If secret does not exist, let's create a new name
+	if plan.Status.Secret == nil {
+		secret.ObjectMeta.Name = req.Name // TODO: maybe introduce a hash of content?
+		secret.ObjectMeta.Namespace = req.Namespace
+	} else {
+		err := r.Get(ctx, types.NamespacedName{
+			Namespace: plan.Status.Secret.Namespace,
+			Name:      plan.Status.Secret.Name,
+		}, &secret)
+		if client.IgnoreNotFound(err) != nil { // Unexpected error
+			r.Recorder.Event(&plan, corev1.EventTypeWarning, "Problem", fmt.Sprintf("Checking owned secret failed with: %v", err))
+			return ctrl.Result{}, err
+		} else if err != nil {
+			// Not found so let's reset the reference and let's re-create it
+			secret.ObjectMeta.Name = plan.Status.Secret.Name
+			secret.ObjectMeta.Namespace = plan.Status.Secret.Namespace
+			plan.Status.Secret = nil
+		}
+	}
+	// Let's compute the content of the secret
+	if secret.Data == nil {
+		secret.Data = map[string][]byte{}
+	}
+	raw, err := json.Marshal(&plan)
+	if err != nil {
+		// TODO: the follow can potentially be used to extract information from the outputted json \o/
+		r.Recorder.Event(&plan, corev1.EventTypeWarning, "Problem", fmt.Sprintf("Unable to marshal plan: %v", err))
+		return ctrl.Result{}, err
+	}
+	secret.Data[secretFieldName] = raw
+	// Finally create or update the secret
+	if plan.Status.Secret != nil {
+		r.Recorder.Event(&plan, corev1.EventTypeNormal, "Info", "Updating secret")
+		err = r.Update(ctx, &secret)
+	} else {
+		r.Recorder.Event(&plan, corev1.EventTypeNormal, "Info", "Creating secret")
+		err = r.Create(ctx, &secret)
+	}
+	if err != nil {
+		log.Error(err, "failed to create or update secret")
+		r.Recorder.Event(&plan, corev1.EventTypeWarning, "Problem", fmt.Sprintf("Update or creation of secret failed with: %v", err))
+		return ctrl.Result{}, err
+	}
+	// Let's make sure to store the reference
+	secretRef, err := ref.GetReference(r.Scheme, &secret)
+	if err != nil {
+		log.Error(err, "failed to get secret reference")
+		r.Recorder.Event(&plan, corev1.EventTypeWarning, "Problem", fmt.Sprintf("Failed to get secret reference: %v", err))
+		return ctrl.Result{}, err
+
+	}
+	plan.Status.Secret = secretRef
+
 	// TODO: create or update cronjob
 	// TODO: if default destination is used, check if additional resources (e.g. secret) should be created
 
