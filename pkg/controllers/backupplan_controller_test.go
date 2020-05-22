4bf6dd4fce5be4ec3f83f2b7e6353eeb2cc45f2e
Jan Steffen <jan.steffen@finleap.com>
2020-05-22 08:17:34 +0200

    Add tests for consul again

--- pkg/controllers/backupplan_controller_test.go
+++ pkg/controllers/backupplan_controller_test.go
@@ -41,6 +41,37 @@ const (
 )
 
 type UpdateMongoDBBackupPlanFunc = func(spec *backupv1alpha1.MongoDBBackupPlan)
+type UpdateConsulBackupPlanFunc = func(spec *backupv1alpha1.ConsulBackupPlan)
+
+func newConsulBackupPlan(namespace string, updates ...UpdateConsulBackupPlanFunc) *backupv1alpha1.ConsulBackupPlan {
+	plan := &backupv1alpha1.ConsulBackupPlan{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      newTestName(),
+		},
+		Spec: backupv1alpha1.BackupPlanSpec{
+			Schedule:              "* * * * *",
+			ActiveDeadlineSeconds: 3600,
+			Retention:             2,
+			Destination: &backupv1alpha1.Destination{
+				S3: &backupv1alpha1.S3{
+					Endpoint:        "localhost:8000",
+					Bucket:          "test",
+					UseSSL:          false,
+					AccessKeyID:     "a",
+					SecretAccessKey: "b",
+				},
+			},
+		},
+		ConsulSpec: backupv1alpha1.ConsulBackupPlanSpec{
+			Address: "localhost:27017",
+		},
+	}
+	for _, f := range updates {
+		f(plan)
+	}
+	return plan
+}
 
 func newMongoDBBackupPlan(namespace string, updates ...UpdateMongoDBBackupPlanFunc) *backupv1alpha1.MongoDBBackupPlan {
 	plan := &backupv1alpha1.MongoDBBackupPlan{
@@ -70,6 +101,12 @@ func newMongoDBBackupPlan(namespace string, updates ...UpdateMongoDBBackupPlanFu
 	for _, f := range updates {
 		f(plan)
 	}
+	return plan
+}
+
+func mustCreateNewConsulBackupPlan(namespace string, updates ...UpdateConsulBackupPlanFunc) *backupv1alpha1.ConsulBackupPlan {
+	plan := newConsulBackupPlan(namespace, updates...)
+	Expect(k8sClient.Create(context.Background(), plan)).Should(Succeed())
 	return plan
 }
 
@@ -299,5 +336,81 @@ fi
 			}
 		}
 		Expect(k8sClient.Delete(ctx, plan)).Should(Succeed())
+	})
+})
+
+var _ = Describe("ConsulBackupPlanReconciler", func() {
+	ctx := context.Background()
+	namespace := ""
+
+	BeforeEach(func() {
+		namespace = mustCreateNamespace()
+	})
+	AfterEach(func() {
+		mustDeleteNamespace(namespace)
+	})
+
+	It("can create ConsulBackupPlans", func() {
+		Context("with missing data", func() {
+			Expect(k8sClient.Create(ctx, &backupv1alpha1.ConsulBackupPlan{})).ShouldNot(Succeed())
+		})
+		Context("with valid data", func() {
+			plan := mustCreateNewConsulBackupPlan(namespace)
+			defer mustRemoveFinalizers(plan)
+		})
+	})
+	It("can process ConsulBackupPlans", func() {
+		Context("which are just created", func() {
+			plan := mustCreateNewConsulBackupPlan(namespace)
+			defer mustRemoveFinalizers(plan)
+			res := mustReconcile(plan)
+			Expect(res.Requeue).To(Equal(false))
+		})
+		Context("which were deleted", func() {
+			plan := mustCreateNewConsulBackupPlan(namespace)
+			defer func() {
+				// If this test fails, we need to make sure the finalizers are removed
+				if err := k8sClient.Get(ctx, namespacedName(plan), plan); err == nil {
+					mustRemoveFinalizers(plan)
+				}
+			}()
+			res := mustReconcile(plan)
+			Expect(res.Requeue).To(Equal(false))
+			Expect(k8sClient.Delete(ctx, plan)).Should(Succeed())
+			Expect(k8sClient.Get(ctx, namespacedName(plan), plan)).Should(Succeed())
+			res = mustReconcile(plan)
+			Expect(res.Requeue).To(Equal(false))
+		})
+	})
+	DescribeTable("can process ConsulBackupPlans multiple times",
+		func(count int) {
+			plan := mustCreateNewConsulBackupPlan(namespace)
+			defer mustRemoveFinalizers(plan)
+			for i := 0; i < count; i++ {
+				res := mustReconcile(plan)
+				Expect(res.Requeue).To(Equal(false))
+			}
+		},
+		Entry("twice", 2),
+		Entry("three times", 3),
+		Entry("five times", 5),
+	)
+	It("creates relevant Secret", func() {
+		plan := mustCreateNewConsulBackupPlan(namespace)
+		defer mustRemoveFinalizers(plan)
+		res := mustReconcile(plan)
+		Expect(res.Requeue).To(Equal(false))
+		Expect(k8sClient.Get(ctx, namespacedName(plan), plan)).Should(Succeed())
+		var secret corev1.Secret
+		Expect(k8sClient.Get(ctx, types.NamespacedName{
+			Namespace: plan.Status.Secret.Namespace,
+			Name:      plan.Status.Secret.Name,
+		}, &secret)).Should(Succeed())
+		Expect(secret.Data).NotTo(BeNil())
+		raw, ok := secret.Data[secretFieldName]
+		Expect(ok).To(Equal(true))
+		var content backupv1alpha1.ConsulBackupPlan
+		Expect(json.Unmarshal(raw, &content)).Should(Succeed())
+		Expect(content.Spec).To(Equal(plan.Spec))
 	})
 })
