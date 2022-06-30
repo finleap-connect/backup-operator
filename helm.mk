7d04f490ee2392f63c6413769d877967f734f9c0
Jan Steffen <jan.steffen@finleap.com>
2022-06-30 11:35:35 +0200

    Fix helm make
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- helm.mk
+++ helm.mk
@@ -5,7 +5,9 @@ HELM_VALUES_FILE            ?= charts/backup-operator/values.yaml
 HELM_OUTPUT_DIR             ?= tmp/helm
 HELM_REGISTRY               ?= https://finleap-connect.github.io/charts
 HELM_REGISTRY_ALIAS         ?= finleap-connect
-HELM_RELEASE                ?= vaop
+HELM_RELEASE                ?= bo
+
+YQ ?= yq
 
 .PHONY: template-clean dependency-update install uninstall template docs
 
@@ -37,7 +39,6 @@ helm-add-finleap: ## add finleap helm chart repo
 helm-set-version-all:
 	@find $(HELM_CHART_DIR) -name 'Chart.yaml' -exec $(YQ) e --inplace '.version = "$(VERSION)"' {} \;
 	@find $(HELM_CHART_DIR) -name 'Chart.yaml' -exec $(YQ) e --inplace '.appVersion = "$(VERSION)"' {} \;
-	@find $(HELM_CHART_DIR) -name 'Chart.yaml' -exec $(YQ) e --inplace '(.dependencies.[].version | select(. == "0.0.1-local")) |= "$(VERSION)"' {} \;
 
 helm-docs: ## update the auto generated docs of all helm charts
 	@docker run --rm --volume "$(PWD):/helm-docs" -u $(shell id -u) jnorwood/helm-docs:v1.4.0 --template-files=./README.md.gotmpl
